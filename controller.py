6296a77a517cfcaa7321c1f3f34b5d4c4fdf2de7
gakki99 <gakki99@hotmail.co.jp>
2014-12-30 00:26:55 +0900

    bug fix

--- controller.py
+++ controller.py
@@ -29,6 +29,13 @@ class Pose:
 class Joint:
     def __init__(self, j1 = 0.0, j2 = 0.0, j3 = 0.0, j4 = 0.0, j5 = 0.0, j6 = 0.0):
         self.data = [j1, j2, j3, j4, j5 ,j6]
+    def __eq__(self, j):
+        for i in range(6):
+            if np.abs(self.data[i] - j.data[i]) > Kinematics.EPS:
+                return False
+        return True
+    def __ne__(self, j):
+        return not self == j
     def max_diff(self, j):
         max = 0.0
         for i in range(6):
@@ -63,7 +70,7 @@ class Joint:
 
 class Kinematics:
     EKinErr = enum.Enum("EKinErr", "none out_of_range no_solution")
-    EPS = np.finfo(np.float).eps
+    EPS = 0.1 ** 12
 
     def __init__(self, la = 30.0, lb = 0.0, lc = 40.0, ld = 0.0, le = 0.0, lf  = 30.0, lg = 30.0, joint_limit = 150.0):
         self.la = la
@@ -76,7 +83,7 @@ class Kinematics:
         self.joint_limit_deg = joint_limit + Kinematics.EPS
         self.joint_limit_rad = np.deg2rad(joint_limit) + Kinematics.EPS
     
-    def __get_t03(self, j):
+    def __get_t03_(self, j):
         s23 = np.sin(j.j2() + j.j3())
         c23 = np.cos(j.j2() + j.j3())
         lbd = self.lb - self.ld
@@ -90,39 +97,50 @@ class Kinematics:
         px = c1 * lcc2plec23mlfs23 - lbd * s1        
         py = s1 * lcc2plec23mlfs23 + lbd * c1        
         pz = -(self.lc * s2)-(self.le * s23)-(self.lf * c23)
-        t03 = np.matrix([ 
+        t03_ = np.matrix([ 
                 [ c1*c23,  s1, -c1*s23,  px ],
                 [ s1*c23, -c1, -s1*s23,  py ],
                 [   -s23, 0.0,    -c23,  pz ],
                 [    0.0, 0.0,     0.0, 1.0 ]])
-        return t03
- 
-    def __get_t36(self,j):
-        s4 = np.sin(j.j4())
-        s5 = np.sin(j.j5())
-        s6 = np.sin(j.j6())
-        c4 = np.cos(j.j4())
-        c5 = np.cos(j.j5())
-        c6 = np.cos(j.j6())
+        return t03_
 
+    @classmethod
+    def get_rx(cls, x):
+        sx = np.sin(x)
+        cx = np.cos(x)
         rx = np.matrix([
                 [ 1.0, 0.0, 0.0, 0.0 ],
-                [ 0.0,  c4, -s4, 0.0 ],
-                [ 0.0,  s4,  c4, 0.0 ],
+                [ 0.0,  cx, -sx, 0.0 ],
+                [ 0.0,  sx,  cx, 0.0 ],
                 [ 0.0, 0.0, 0.0, 1.0 ]])
+        return rx
+ 
+    @classmethod
+    def get_ry(cls, y):
+        sy = np.sin(y)
+        cy = np.cos(y)
         ry = np.matrix([
-                [  c5, 0.0,  s5, 0.0 ],
+                [  cy, 0.0,  sy, 0.0 ],
                 [ 0.0, 1.0, 0.0, 0.0 ],
-                [ -s5, 0.0,  c5, 0.0 ],
+                [ -sy, 0.0,  cy, 0.0 ],
                 [ 0.0, 0.0, 0.0, 1.0 ]])
+        return ry
+
+    @classmethod
+    def get_rz(cls, z):
+        sz = np.sin(z)
+        cz = np.cos(z)
         rz = np.matrix([
-                [  c6, -s6, 0.0, 0.0 ], 
-                [  s6,  c6, 0.0, 0.0 ],
+                [  cz, -sz, 0.0, 0.0 ], 
+                [  sz,  cz, 0.0, 0.0 ],
                 [ 0.0, 0.0, 1.0, 0.0 ],
                 [ 0.0, 0.0, 0.0, 1.0 ]])
-        tmp = np.dot(rz,ry)
-        t36 = np.dot(tmp,rx)
-        return t36
+        return rz
+
+    def __get_t36_(self, j):
+        tmp = np.dot(Kinematics.get_rz(j.j4()), Kinematics.get_ry(j.j5()))
+        t36_ = np.dot(tmp, Kinematics.get_rz(j.j6()))
+        return t36_
    
     def __get_t6h(self):
         t6h = np.matrix([
@@ -133,20 +151,25 @@ class Kinematics:
         return t6h
         
     def forward(self, j):
-        t03 = self.__get_t03(j.deg2rad())
+        t03_ = self.__get_t03_(j.deg2rad())
         j_ = j.deg2rad()
         j_.data[4] = -j_.data[4]
-        t36 = self.__get_t36(j_)
-        t06 = np.dot(t03, t36)
-        tb6 = t06
-        tb6[(2,3)] = tb6[(2,3)] + self.la
+        t36_ = self.__get_t36_(j_)
+        t06 = np.dot(t03_, t36_)
+        la = np.matrix([
+                [  0.0, 0.0, 0.0,     0.0 ],
+                [  0.0, 0.0, 0.0,     0.0 ],
+                [  0.0, 0.0, 0.0, self.la ],
+                [  0.0, 0.0, 0.0,     0.0 ]])
+        tb6 = t06 + la
         t6h = self.__get_t6h()
         tbh = np.dot(tb6, t6h)
-        Logger.log(Logger.ELogLevel.TRACE, "T(0->3) =\n%s", t03)
-        Logger.log(Logger.ELogLevel.TRACE, "T(3->6) =\n%s", t36)
-        Logger.log(Logger.ELogLevel.TRACE, "T(B->6) =\n%s", tb6)
-        Logger.log(Logger.ELogLevel.TRACE, "T(6->H) =\n%s", t6h)
-        Logger.log(Logger.ELogLevel.TRACE, "T(B->H) =\n%s", tbh)
+        Logger.log(Logger.ELogLevel.TRACE, "t03_=\n%s", t03_)
+        Logger.log(Logger.ELogLevel.TRACE, "t36_=\n%s", t36_)
+        Logger.log(Logger.ELogLevel.TRACE, "t06 =\n%s", t06)
+        Logger.log(Logger.ELogLevel.TRACE, "tb6 =\n%s", tb6)
+        Logger.log(Logger.ELogLevel.TRACE, "t6h =\n%s", t6h)
+        Logger.log(Logger.ELogLevel.TRACE, "tbh =\n%s", tbh)
         return Kinematics.mat2pose(tbh)
 
     @classmethod
@@ -199,9 +222,18 @@ class Kinematics:
         t6h = self.__get_t6h()
         inv_t6h = np.linalg.inv(t6h)
         tb6 = np.dot(tbh, inv_t6h)
-        t06 = tb6
-        t06[(2,3)] = t06[(2,3)] - self.la
+        la = np.matrix([
+                [  0.0, 0.0, 0.0,     0.0 ],
+                [  0.0, 0.0, 0.0,     0.0 ],
+                [  0.0, 0.0, 0.0, self.la ],
+                [  0.0, 0.0, 0.0,     0.0 ]])
+        t06 = tb6 - la
         p = Kinematics.mat2pose(t06)
+
+        Logger.log(Logger.ELogLevel.TRACE, "t06 =\n%s", t06)
+        Logger.log(Logger.ELogLevel.TRACE, "tb6 =\n%s", tb6)
+        Logger.log(Logger.ELogLevel.TRACE, "t6h =\n%s", t6h)
+        Logger.log(Logger.ELogLevel.TRACE, "tbh =\n%s", tbh)
 
         lbd = self.lb - self.ld
         px2ppy2 = p.px() ** 2.0 + p.py() ** 2.0
@@ -219,7 +251,7 @@ class Kinematics:
         else:
             j1.append(Kinematics.nomalize_rad(np.arctan2(p.py(), p.px()) - np.arctan2(a,  np.sqrt(1.0 - a ** 2.0))))
             j1.append(Kinematics.nomalize_rad(np.arctan2(p.py(), p.px()) - np.arctan2(a, -np.sqrt(1.0 - a ** 2.0))))
-        
+    
         px2ppy2ppz2 = px2ppy2 + p.pz() ** 2.0
 
         if px2ppy2ppz2 < Kinematics.EPS:
@@ -231,9 +263,10 @@ class Kinematics:
                 if err is Kinematics.EKinErr.none:
                     sol123.extend(tmp)
 
+        Logger.log(Logger.ELogLevel.TRACE, "sol123 = %s", np.rad2deg(sol123)) 
+
         if len(sol123) == 0:
             return Kinematics.EKinErr.no_solution, None
-
         for i in range(len(sol123)):
             err, sol456 = self.__inverse456(t06, Joint(sol123[i][0], sol123[i][1], sol123[i][2]))
             if err is Kinematics.EKinErr.none:    
@@ -244,9 +277,12 @@ class Kinematics:
         elif len(sol) == 1:
             return Kinematics.EKinErr.none, sol[0]
 
+        Logger.log(Logger.ELogLevel.TRACE, "joint = %s", joint) 
+        Logger.log(Logger.ELogLevel.TRACE, "sol[0] = %s", sol[0]) 
         max_diff = sol[0].max_diff(joint)
         max_diff_id = 0
         for i in range(1,len(sol)):
+            Logger.log(Logger.ELogLevel.TRACE, "sol[%d] = %s", i, sol[i]) 
             diff = sol[i].max_diff(joint)
             if max_diff > diff:
                 max_diff = diff
@@ -267,11 +303,11 @@ class Kinematics:
     def __inverse456(self, t06, j):
         sol = []
         j4 = []
-        t03 = self.__get_t03(j)
-        t36 = np.dot(np.linalg.inv(t03), t06)
-        ax = t36[(0,2)]
-        ay = t36[(1,2)]
-        az = t36[(2,2)]
+        t03_ = self.__get_t03_(j)
+        t36_ = np.dot(np.linalg.inv(t03_), t06)
+        ax = t36_[(0,2)]
+        ay = t36_[(1,2)]
+        az = t36_[(2,2)]
         ax2pay2 = (ax**2.0 + ay**2.0)
         
         if ax2pay2 < Kinematics.EPS:
@@ -279,38 +315,40 @@ class Kinematics:
         else:
             j4.append(Kinematics.nomalize_rad(np.arctan(ay/ax)))
             j4.append(Kinematics.nomalize_rad(np.arctan(ay/ax) + np.pi))
-        
+ 
+        Logger.log(Logger.ELogLevel.TRACE, "j4 = %s", np.rad2deg(j4)) 
+
         for i in range(len(j4)):
             if np.abs(j4[i]) <= self.joint_limit_rad:
                 j5 = None
                 j6 = None
                 if ax2pay2 > Kinematics.EPS:
-                    j5, j6 = self.__inverse56(t36, j4[i])
+                    j5, j6 = self.__inverse56(t36_, j4[i])
                 else:
-                    j5, j6 = self.__inverse56(t36, j4[i])
+                    j5, j6 = self.__inverse56(t36_, j4[i])
                     j5 = Kinematics.nomalize_rad(np.pi / 2.0 * ( 1.0 - az))
 
                 if np.abs(j5) <= self.joint_limit_rad and np.abs(j6) <= self.joint_limit_rad:
-                    sol.append(Joint(j.j1(),j.j2(),j.j3(),j4[i],j5,j6).rad2deg())
+                    sol.append(Joint(j.j1(),j.j2(),j.j3(),j4[i],-j5,j6).rad2deg())
 
         if len(sol) == 0:
             return Kinematics.EKinErr.out_of_range, None
 
         return Kinematics.EKinErr.none, sol
 
-    def __inverse56(self, t36, j4):
-        ax = t36[(0,2)]
-        ay = t36[(1,2)]
-        az = t36[(2,2)]
-        sx = t36[(0,1)]
-        sy = t36[(1,1)]
-        nx = t36[(0,0)]
-        ny = t36[(1,0)]
+    def __inverse56(self, t36_, j4):
+        ax = t36_[(0,2)]
+        ay = t36_[(1,2)]
+        az = t36_[(2,2)]
+        sx = t36_[(0,1)]
+        sy = t36_[(1,1)]
+        nx = t36_[(0,0)]
+        ny = t36_[(1,0)]
         c4 = np.cos(j4)
         s4 = np.sin(j4)
 
         s = np.arctan2(ax*c4 + ay*s4, az)
-        j5 = -s
+        j5 = s
         j6 = np.arctan2(-nx*s4 + ny*c4, -sx*s4 + sy*c4)
         return (Kinematics.nomalize_rad(j5), Kinematics.nomalize_rad(j6))
  
@@ -359,7 +397,8 @@ class Controller:
     def tenth_deg(cls, deg):
         return int(round(deg * 10.0, 0))
 
-    def __init__(self, controll_period = 20.0, max_speed = 240.0 / 1000.0):
+    def __init__(self, controll_period = 20.0, max_speed = 240.0 / 1000.0, loglv = Logger.ELogLevel.DEBUG):
+        Logger.level = loglv 
         self.max_speed = max_speed # deg per msec
         self.controll_period = controll_period # msec
         self.status = {}
@@ -407,7 +446,7 @@ class Controller:
                     Logger.log(Logger.ELogLevel.INFO_,"move_ptp, target_pose = %s", target)
                     err, target = self.kinematics.inverse(target, current)
                     if err is not Kinematics.EKinErr.none:
-                        Logger.log(Logger.ELogLevel.ERROR, "inverse kinematics error = %s" % err.name)
+                        Logger.log(Logger.ELogLevel.ERROR, "inverse kinematics error = %s", err.name)
                         self.__callback(msg, err)
                         continue
 
@@ -454,11 +493,13 @@ class Controller:
         self.status[Controller.EStatKey.pose] = self.kinematics.forward(self.status[Controller.EStatKey.joint])
         Logger.log(Logger.ELogLevel.INFO_, "update_pose, joint = %s", self.status[Controller.EStatKey.joint])
         Logger.log(Logger.ELogLevel.INFO_, "update_pose, pose = %s", self.status[Controller.EStatKey.pose])
-        err, sol = self.kinematics.inverse(self.status[Controller.EStatKey.pose])
-        if err is Kinematics.EKinErr.none:
-            Logger.log(Logger.ELogLevel.INFO_, "inverse kinematics solution check = %s" % sol)
-        else:
-            Logger.log(Logger.ELogLevel.ERROR, "inverse kinematics error = %s" % err.name)
+        if Logger.level.value >= Logger.ELogLevel.DEBUG.value:
+            err, sol = self.kinematics.inverse(self.status[Controller.EStatKey.pose], self.status[Controller.EStatKey.joint])
+            if err is Kinematics.EKinErr.none:
+                if sol != self.status[Controller.EStatKey.joint]:
+                    Logger.log(Logger.ELogLevel.ERROR, "inverse kinematics solution does not correspond, result = %s", sol)
+            else:
+                Logger.log(Logger.ELogLevel.ERROR, "inverse kinematics error = %s", err.name)
 
     def __callback(self, msg, value = None):
         if msg[Controller.EMsgKey.callback] is not None:
@@ -538,8 +579,9 @@ if __name__ == '__main__':
     c.move_ptp(Joint(   0.0,   0.0, 0.0, 0.0,   0.0, 0.0))
     c.move_ptp(Pose(  30.0,   0.0,  30.0, 180.0,   0.0,   0.0))
     c.move_ptp(Pose(  30.0,   0.0,  30.0,  90.0,   0.0,   0.0))
-    #c.move_ptp(Pose(  30.0,  30.0,  30.0,  90.0,   0.0, -90.0))
-    #c.move_ptp(Pose(  30.0,  30.0,  30.0,  90.0, 120.0, -90.0))
+    c.move_ptp(Pose(  30.0,  30.0,  30.0,  90.0,   0.0, -90.0))
+    c.move_ptp(Pose(  30.0,  30.0,  30.0,  90.0, 120.0, -90.0))
+    c.move_ptp(Pose(  30.0,  30.0,  30.0, -90.0,-120.0,  90.0))
     #c.move_ptp(Joint(  30.0,  30.0, 0.0, 0.0,  30.0, 0.0))
     #c.move_ptp(Joint( -60.0, -60.0, 0.0, 0.0, -60.0, 0.0))
     #c.move_ptp(Joint(  90.0,  90.0, 0.0, 0.0,  90.0, 0.0))
